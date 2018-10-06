0feec56c37c4f3f76673fc7af58f946a986448d6
Unknown <zckd78@gmail.com>
2018-10-05 22:49:48 -0700

    Pixel generation!
    Can now dynamically generate a pixel board. Added styles for big and small pixels, comments for the functions, coordinates on the pixels, and click event handlers (Only a test for now).

--- code.js
+++ code.js
@@ -1,15 +1,14 @@
 
-
+// Generates the Table based on the x and y Max args.
 function generateTable(xMax,yMax){
-
-
+	
 	for (var y = 0; y < yMax; y++) {
+		// Generates the Table, one row at a time
 		addRow(xMax, y);
 	}
-
 }
 
-// Creates a new row and set
+// Creates a new row and set the pixels in place.
 function addRow(xMax, y) {
     // Access the elements from the DOM
     let table = document.getElementById("GameTable");
@@ -19,10 +18,37 @@ function addRow(xMax, y) {
     
     // row.insertCell(0).innerHTML= '<div id="'+id+'"></div>';
     for (var x = 0; x < xMax; x++) {
-		row.insertCell(x).innerHTML= '<div class="block" id="x'+x+'y'+x+'"></div>';
+
+    	let tagID = 'x'+x+'y'+y;
+    	let coords = x+','+y;
+    	/* --------------------------------------------------
+    	  TODO Continue to update the div tags from HERE!
+    	  This space is what places the pixels in a line.
+    	 -------------------------------------------------- */ 
+    	if(xMax == 7){
+			row.insertCell(x).innerHTML= '<div onclick="testLeftClick(this)" class="pixel_large" id="' + tagID + '">' + coords + '</div>';
+    	} else if (xMax == 13){
+			row.insertCell(x).innerHTML= '<div onclick="testLeftClick(this)" class="pixel_small" id="' + tagID + '">' + coords + '</div>';
+    	}
 	}
 }
 
+/* 	Since the pixels pass themselves as (this), we can use their properties, and access
+	their children through the DOM.
+*/
+function testLeftClick(pixel){
+	// I'm alerting the id, although we have access to more.
+	alert(pixel.id+" was left clicked");
+}
+
+function testRightClick(id){
+	// I'm alerting the id, although we have access to more.
+	alert(pixel.id+" was right clicked");
+}
+
+/*
+Sourced from https://blackboard.learn.fresnostate.edu/bbcswebdav/pid-2341195-dt-content-rid-49956440_1/courses/CSCI130-02-76194-2187/class_javascript_dom_dynatable.html
+*/
 
 function deleteRow(obj) {
     let index = obj.parentNode.parentNode.rowIndex;
